61a9ad9be97c39d6853b79999587d395a4afcf10
Mohammad Rizaldi Huzein Prastomo <rizaldi.huzein@gmail.com>
2018-11-30 21:48:50 +0700

    update index

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 session_start();
@@ -17,6 +16,8 @@ $dir = explode("htdocs",$root)[1];
 
 $path = explode("?", $_SERVER["REQUEST_URI"])[0];
 
+$db = include "$root/app/control/database.php";
+$GLOBALS['db']=$db;
 
 if (!isset($_SESSION['admin'])) {
     $_SESSION['admin']['loggedin'] =false;
@@ -33,9 +34,12 @@ function redirect($path = "/")
     }
 }
 
-
-$db = include "$root/app/control/database.php";
-
+function cek($nomor){
+    $query = "SELECT `status` FROM `pc` where id='$nomor'";
+    $hasil = $GLOBALS['db']->query($query);
+    $hasil = mysqli_fetch_assoc($hasil);
+    return $hasil['status'];
+}
 
 if (file_exists("$app/control$path.php")) {
     include "$app/control$path.php";
@@ -73,88 +77,4 @@ if (file_exists("$app/view$path.php")) {
     
     include "$root/app/view/default.php";
 }
-=======
-<?php
-
-session_start();
-
-error_reporting(E_ERROR | E_PARSE);
-
-
-
-$root = str_replace("\\", "/", __DIR__);
-$app = $root."/app";
-$public = $root."/public";
-
-
-define('FPDF_FONTPATH',"$app/control/pdf/font/");
-
-$dir = explode("htdocs",$root)[1];
-// isi $dir = /CAMIN/
-
-$path = explode("?", $_SERVER["REQUEST_URI"])[0];
-// REQUEST URI /CAMIN/request - ? - login
-
-
-if (!isset($_SESSION['admin'])) {
-    $_SESSION['admin']['loggedin'] =false;
-}
-
-function redirect($path = "/")
-{
-    $path = ltrim($path, "/");
-    if (headers_sent()) {
-        die("<script>window.location='/$path';</script>");
-    } else {
-        header("Location: /$path");
-        header($_SERVER["SERVER_PROTOCOL"] . " 302 Found", true, 302);
-    }
-}
-
-
-$db = include "$root/app/control/database.php";
-
-
-//cek ada control
-//misal login control user
-// CAMIN/user
-if (file_exists("$app/control$path.php")) {
-    include "$app/control$path.php";
-} else {
-   
-    $partial_path = "/" . strtok($path, "/");
-    do{
-        $partial_path = "/".$new_token;
-        $absolute_path = "$app/control$partial_path.php";
-        if (file_exists($absolute_path)) {            
-            include $absolute_path;
-            break;
-        }
-        $partial_path .= "/";
-    }while($new_token = strtok("/"));
-
-}
-
-
-$prefix = explode("/", $path)[1];
-
-
-//include tampilan
-if (file_exists("$app/view$path.php")) {
-    include "$app/view$path.php";
-} else {
-    $partial_path = "/" . strtok($path, "/");
-    do{
-        $partial_path = "/".$new_token;
-        $absolute_path = "$app/view$partial_path.php";
-        if (file_exists($absolute_path)) {            
-            include $absolute_path;
-            exit;
-        }
-        $partial_path .= "/";
-    }while($new_token = strtok("/"));
-    
-    include "$root/app/view/default.php";
-}
->>>>>>> master
 ?>
